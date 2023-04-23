33d8ea360fb6b9f1ec393321d11f8112bf16556e
matfmar <marzec-mateusz@wp.pl>
2023-04-23 20:03:55 +0200

    added out

---
+++ out/production/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.sawannaniz.databaseclient.ctrl"},{"l":"net.sawannaniz.databaseclient.dbutils"},{"l":"net.sawannaniz.databaseclient.gui"}]
